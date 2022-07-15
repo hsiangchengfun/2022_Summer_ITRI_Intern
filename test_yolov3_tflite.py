875227bd2e0ee460ed87c6563464bd650cda2404
hsiang <50895141+hsiangchengfun@users.noreply.github.com>
2022-07-15 09:56:10 +0800

    ResNet50_yolov3

---
+++ test_yolov3_tflite.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from tensorflow.python.saved_model import tag_constants
+import core.utils as utils
+from PIL import Image
+import cv2
+import numpy as np
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+from core.yolov4 import filter_boxes
+
+#input- input_1
+#output- Identity
+image_path = "kite.jpg"
+input_size = 416
+tflite_model = "yolov3-416-int8_tf2_3_0.tflite"
+if __name__ == '__main__':
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    original_image = cv2.imread(image_path)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+    image_data = cv2.resize(original_image, (input_size, input_size))
+    image_data = image_data / 255.
+
+    images_data = []
+    for i in range(1):
+        images_data.append(image_data)
+    images_data = np.asarray(images_data).astype(np.float32)
+    interpreter = tf.lite.Interpreter(model_path=tflite_model)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    interpreter.set_tensor(input_details[0]['index'], images_data)
+    interpreter.invoke()
+    pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+
+
+    boxes, pred_conf = filter_boxes(pred[0], pred[1], score_threshold=0.25,
+                                    input_shape=tf.constant([input_size, input_size]))
+    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=0.45,
+        score_threshold=0.25
+    )
+    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+    image = utils.draw_bbox(original_image, pred_bbox)
+    image = Image.fromarray(image.astype(np.uint8))
+    image.save('result.jpg')
+    image.show()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    cv2.imwrite("kite_yolov3_result.jpg", image)
+
+
