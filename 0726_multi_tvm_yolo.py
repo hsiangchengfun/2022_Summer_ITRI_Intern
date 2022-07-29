3923ff28c1293d804a0530d9844fc1f7e623bb67
hsiang <50895141+hsiangchengfun@users.noreply.github.com>
2022-07-29 15:00:15 +0800

    Add files via upload

--- 0726_multi_tvm_yolo.py
+++ 0726_multi_tvm_yolo.py
@@ -51,7 +51,7 @@ import cv2
 import glob
 img_list = []
 org_list = []
-for img in glob.glob("yolo_images/*.jpg"):
+for img in sorted(glob.glob("yolo_images/*.jpg")):
 
     original_image = cv2.imread(img)
     original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
@@ -94,6 +94,72 @@ with transform.PassContext(opt_level=3):
     lib = relay.build(mod, target, params=params)
 
 
+
+
+########################################################
+#for num to class
+
+
+import cv2
+import glob
+import shutil
+
+
+file_list=[]
+i =  0
+l =[]
+std_dict={}
+with open("coco.names") as std:
+
+	classes = std.read().splitlines()#readlines(end="")#.split(' ') 
+
+"""
+print((classes[5]))
+print(len(classes[0]))
+"""
+
+for i in range(80):
+	std_dict[classes[i]] = i 
+
+#######################################################
+#read answer
+
+
+ans_d_list=[]
+ll =[]
+tmp=[]
+for i in range(50):
+	for j in range(90):
+		tmp.append(0)
+	ll.append(tmp)
+	tmp=[]
+print(len(ll))
+print(len(ll[20]))
+y=0
+for f in sorted(glob.glob("groundtruths/*.txt")):
+	
+	
+	with open(f) as ff:
+		ans = (ff.read().splitlines())
+
+		for j in range(len(ans)):
+			t = str(ans[j])
+			t = t.split(" ")
+			
+			if t[0] in std_dict:
+				ll[y][std_dict[t[0]]] += 1
+			else:
+				std_dict[t[0]] = 1
+
+	
+
+	y+=1
+
+# ll[x] is the txt[x]'s answer# ll[a][b] => a image that have ll[a][b]s b
+#######################################################
+
+
+
 import tvm
 from tvm import te
 from tvm.contrib import graph_executor as runtime
@@ -101,7 +167,17 @@ from tvm.contrib import graph_executor as runtime
 module = runtime.GraphModule(lib["default"](tvm.cpu()))
 # Feed input data
 i=0
+used=[]
+usenum=0
+num = []
+corr=[]
+IOU=0
+for i in range(80):
+	used.append(0)
+	num.append(0)
+	corr.append(0)
 for i in range(50):
+
 	module.set_input(input_tensor, tvm.nd.array(img_list[i]))
 
 	module.run()
@@ -123,6 +199,34 @@ for i in range(50):
 	pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
 	images = utils.draw_bbox(org_list[i], pred_bbox)
 	
+	a=0
+	for k in range(len(classes[0])):
+		if(classes[0][k]!=0):
+			a+=1
+			used[int(classes[0][k])]+=1
+		
+		num[int(classes[0][k])]+=1
+	num[0]=0
+	q=0
+	for k in range(len(scores[0])):
+		if((scores[0][k])!=0 ):
+			q+=1
+
+			
+	num[0]+=q-a
+	used[int(classes[0][k])]+=q-a
+	#print(num)
+	for y in range(80):
+		if(ll[i][y] > num[y]  ):
+			corr[y]+=num[y];
+		else:
+			corr[y]+=ll[i][y];
+	
+	for k in range(80):
+		num[k]=0
+	
+	
+	#print(ans)
 	images = images.astype(np.float32)
 	images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
 
@@ -130,4 +234,35 @@ for i in range(50):
 
 	name = str(i)+".jpg"
 	cv2.imwrite(name, images)
+
+
+for i in range(80):
+	if used[i]!=0:
+		usenum+=1;
+print(corr)
+print(used)
+print("len",len(used))
+print(usenum)
+
+
+AP=[]
+tt=0
+for i in range(80):
+	AP.append(0)
+	if(used[i]!=0):AP[i] = corr[i]/used[i]
+	tt += AP[i]
+
+mAP = tt/usenum 
+
+print("\n")
+print(AP)
+#print("iou_threshold:",iou_threshold,"\n")
+print("mAP: ",mAP)
+
+
+
+
+
+
+
 
